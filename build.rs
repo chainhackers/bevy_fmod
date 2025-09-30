023b24231afbce58303606fe48184123d7450bfd
etsvigun <gene@chainhackers.xyz>
2025-09-30 18:22:12 +0300

    Upgrade bevy_fmod to use FMOD 2.03.09 and improve build script

--- build.rs
+++ build.rs
@@ -1,13 +1,10 @@
 //! This build script is needed to run the examples on Linux.
-//! Feel free to copy this to your own Bevy project.
 //!
-//! Be aware that on Linux you also need to set LD_LIBRARY_PATH to include the libraries.
-//! There are a few ways to do it:
+//! Set the FMOD_SDK_DIR environment variable to point to your FMOD SDK directory.
+//! For example: export FMOD_SDK_DIR=/path/to/fmod/20309_processed
 //!
-//! If you are working with an IDE it's easiest to add this to the run configuration.
-//! For example, in Rust Rover you set `Environment variables` to `LD_LIBRARY_PATH=./fmod/api/core/lib/x86_64:./fmod/api/studio/lib/x86_64`.
-//!
-//! If you are running your executable directly (no IDE, no cargo) see https://www.hpc.dtu.dk/?page_id=1180
+//! You also need to set LD_LIBRARY_PATH to include the libraries at runtime:
+//! export LD_LIBRARY_PATH=$FMOD_SDK_DIR/api/core/lib/x86_64:$FMOD_SDK_DIR/api/studio/lib/x86_64:$LD_LIBRARY_PATH
 
 fn main() {
     #[cfg(target_os = "linux")]
@@ -19,13 +16,38 @@ fn main() {
             target_arch = "arm64".into();
         }
 
-        let api_loc = [
-            format!("./fmod/api/core/lib/{target_arch}"),
-            format!("./fmod/api/studio/lib/{target_arch}"),
-        ];
-        for loc in api_loc {
-            println!("cargo:rustc-link-search={loc}");
-            println!("cargo:rustc-env=LD_RUN_PATH={loc}");
+        // Try to find FMOD libraries using environment variable
+        if let Ok(fmod_dir) = std::env::var("FMOD_SDK_DIR") {
+            let api_loc = [
+                format!("{}/api/core/lib/{}", fmod_dir, target_arch),
+                format!("{}/api/studio/lib/{}", fmod_dir, target_arch),
+            ];
+
+            for loc in &api_loc {
+                // Verify the directory exists
+                if std::path::Path::new(loc).exists() {
+                    println!("cargo:rustc-link-search={}", loc);
+                    println!("cargo:rustc-env=LD_RUN_PATH={}", loc);
+                } else {
+                    println!("cargo:warning=FMOD library directory not found: {}", loc);
+                }
+            }
+        } else {
+            // Fall back to common system locations
+            let system_paths = [
+                "/usr/local/lib",
+                "/usr/lib",
+                "/opt/fmod/lib",
+            ];
+
+            for path in &system_paths {
+                if std::path::Path::new(path).exists() {
+                    println!("cargo:rustc-link-search={}", path);
+                }
+            }
+
+            println!("cargo:warning=FMOD_SDK_DIR environment variable not set. Set it to your FMOD SDK directory.");
+            println!("cargo:warning=Example: export FMOD_SDK_DIR=/path/to/fmod/20309_processed");
         }
     }
 }
